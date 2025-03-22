761c31db4b5e251d55348dfab2cf1c1967654221
clzoc <1055343256@qq.com>
2025-03-22 12:55:49 +0800

    Initial Commit

---
+++ app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by tsunami on 2025/3/22.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ appTests/appTests.swift
@@ -0,0 +1,17 @@
+//
+//  appTests.swift
+//  appTests
+//
+//  Created by tsunami on 2025/3/22.
+//
+
+import Testing
+@testable import app
+
+struct appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
